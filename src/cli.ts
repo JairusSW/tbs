c11eb8e234f5e7755358c7d08b5a40bbfda63afb
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-02-15 17:17:13 -0800

    Generate AssemblyScript methods

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@ import { parseSchema } from "./parser/parser.js";
 import { readFileSync } from "fs";
 import { compileSchema } from "./compiler/compile.js";
 import { Schema } from "./parser/Schema.js";
+import { AssemblyScript } from "./generators/assemblyscript.js";
 const version = "v0.1.0";
 const args = process.argv.slice(2);
 if (args.includes("-h") || !args.length) {
@@ -15,7 +16,7 @@ if (args.includes("-h") || !args.length) {
    ╚═╝   ╚══════╝╚══════╝`.blue);
   console.log("\nUsage: tbs [command] [flags]");
   console.log("\nCommands:");
-  console.log(" - generate -i [in] -o [out] --target [language]\n\tCreate output files for provided schemas compiled to specified language");
+  console.log(" - generate -i [in] -o [out] --[target]\n\tCreate output files for provided schemas compiled to specified language");
   console.log(" - compile -i [in] -o [out]\n\tCompile schemas into their binary form");
 } else if (args.includes("-v")) {
   console.log(version);
@@ -24,14 +25,12 @@ if (args.includes("-h") || !args.length) {
     console.error("No input provided. Cancelling.");
   } else if (args[3] != "-o") {
     console.error("No output provided. Cancelling.");
-  } else if (args[5] != "--target") {
-    console.error("No target flag provided. Cancelling.");
-  } else if (!args[6]) {
-    console.error("No target language specified. Cangelling.");
+  } else if (!args.includes("--assemblyscript")) {
+    console.error("No target language specified. Cancelling.");
   } else {
     const input = args[2]!;
     const output = args[4]!;
-    const target = args[6]!;
+    const target = "assemblyscript"
     console.log(`Running...\n - Target: ${target}\n - Input: ${input}\n - Output: ${output}\n`);
     const file = readFileSync(input).toString();
     const parsed = parseSchema(file);
@@ -39,6 +38,7 @@ if (args.includes("-h") || !args.length) {
     for (let i = 0; i < parsed.keys.length; i++) {
       console.log(` - ${parsed.keys[i]} -> ${parsed.types[i]}`);
     }
+    const generated = new AssemblyScript(parsed.keys, parsed.types);
     console.log("Done.");
   }
 } else if (args[0] == "compile") {
--- src/generators/assemblyscript.ts
+++ src/generators/assemblyscript.ts
@@ -6,4 +6,31 @@ export class AssemblyScript extends Generator {
     public outputText: string = "";
     public keys: string[] = [];
     public types: string[] = [];
+    private serializeStmts: string[] = [];
+    private deserializeStmts: string[] = [];
+    constructor(keys: string[], types: string[]) {
+        super();
+        this.keys = keys;
+        this.types = types;
+        for (let i = 0; i < this.keys.length; i++) {
+            const key = keys[i]!;
+            const type = types[i]!;
+            if (["i8", "u8", "i16", "u16", "i32", "u32", "f32", "i64", "u64", "f64"].includes(type)) {
+                this.serializeStmts.push(`store<${type}>(changetype<usize>(result) + offset + <usize>${this.offset + this.offsetDyn}, data.${key});`);
+                this.deserializeStmts.push(`result.${key} = load<${type}>(changetype<usize>(data) + offset + <usize>${this.offset + this.offsetDyn});`);
+                if (type.endsWith("8")) this.offset++;
+                else if (type.endsWith("16")) this.offset += 2;
+                else if (type.endsWith("32")) this.offset += 4;
+                else if (type.endsWith("64")) this.offset += 8;
+            }
+        }
+        this.serializeStmts = this.serializeStmts.map(v => v.replaceAll(" + <usize>0", "").replaceAll(" + <usize>dynamic.", " + <usize>input."));
+        this.deserializeStmts = this.deserializeStmts.map(v => v.replaceAll(" + <usize>0", "").replaceAll(" + <usize>dynamic.", " + <usize>out."));
+        let serializeFunction = `@inline export function serialize<T>(data: T): ArrayBuffer {\n\tconst result = new ArrayBuffer(${this.offset});\n\t${this.serializeStmts.join("\n\t")}\n\treturn result;\n}`;
+        let deserializeFunction = `@inline export function deserialize<T>(data: ArrayBuffer): T {\n\tconst result = new Vec3();\n\t${this.deserializeStmts.join("\n\t")}\n\treturn result;\n}`;
+        console.log(this.serializeStmts);
+        console.log(this.deserializeStmts);
+        console.log(serializeFunction);
+        console.log(deserializeFunction);
+    }
 }
